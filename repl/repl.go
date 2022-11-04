e147fd4d957d5ca4819f7276fb9c12d5dfec3667
SiNA <Apple@MacBooks-MacBook-Air-2.local>
2022-11-04 19:01:34 +0330

    exit command

--- repl/repl.go
+++ repl/repl.go
@@ -1,12 +1,23 @@
 package repl
 
+import (
+	"net"
+)
+
 func ParseCommands(c string) (string, error) {
 	if c == "salam\n" {
 		return help()
+	} else if c == ".exit\n" {
+		exit()
 	}
 	return "Command Not Found", nil
 }
 
 func help() (string, error) {
 	return "help", nil
+}
+
+func exit() (c net.Conn) {
+	c.Close()
+	return
 }
